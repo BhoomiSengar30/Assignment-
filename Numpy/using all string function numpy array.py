6e2ca5bddeb74654d62af80fc26cc8e509323618
BhoomiSengar30 <101420865+BhoomiSengar30@users.noreply.github.com>
2022-05-30 23:23:37 +0530

    Create using all string function numpy array.py

---
+++ Numpy/using all string function numpy array.py
@@ -0,0 +1,13 @@
+import numpy as np
+a=np.array(["akash","rathore","course"])
+b=" ".join(a)
+#capitalize
+print(np.array(b.capitalize().split()))
+#converted into lowercase
+print(np.array(b.lower().split()))
+#converyted into uppercase
+print(np.array(b.upper().split()))
+#converted into title case
+print(np.array(b.title().split()))
+#converted into swapcase
+print(np.array(b.swapcase().split()))
