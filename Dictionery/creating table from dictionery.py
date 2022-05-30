b0b0faa1669aeb5a77b7ec24be95e5c14b6d4b4d
BhoomiSengar30 <101420865+BhoomiSengar30@users.noreply.github.com>
2022-05-30 22:58:22 +0530

    Create creating table from dictionery.py

---
+++ Dictionery/creating table from dictionery.py
@@ -0,0 +1,3 @@
+d={"Akash":[70,80,90,68,59],"Bhoomi":[70,80,90,68,59],"Anshika":[70,80,90,68,59],"Soumya":[70,80,90,68,59],"Neha":[70,80,90,68,59]}
+for row in zip(*([k]+(v) for k,v in sorted(d.items()))):
+    print(*row,sep="\t")
