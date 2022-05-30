c17f9f38f5ff2b3587059c37d79d88b5cc794fbf
BhoomiSengar30 <101420865+BhoomiSengar30@users.noreply.github.com>
2022-05-30 23:38:44 +0530

    Create WAP to find the area of trapezium.py

---
+++ File handling and modules/WAP to find the area of trapezium.py
@@ -0,0 +1,5 @@
+a=float(input("enter the value of parallel side of trap.:"))
+b=float(input("enter the opposite parallel side of trap:"))
+h=float(input("enter the height of trapazium:"))
+area=1/2*(a+b)*h
+print("area of trapazium is:",area)
